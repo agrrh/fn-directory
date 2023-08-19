7a1b1ab95686a616a9491039c10903c0d22f69a2
Kirill K <kovalev.kirill.a@gmail.com>
2023-08-19 15:13:19 +0300

    chore: test

--- film-reviews-get-metadata/__init__.py
+++ film-reviews-get-metadata/__init__.py
@@ -1,18 +1,18 @@
-# from datetime import datetime
-#
-# from pydantic import BaseModel, Field
-#
-#
-# class Payload(BaseModel):
-#     """
-#     Sample:
-#     {
-#         "name": "update deployment",
-#         "message": "Successfully updated deployment default/wd (karolisr/webhook-demo:0.0.10)",
-#         "createdAt": "2017-07-08T10:08:45.226565869+01:00"
-#     }
-#     """
-#
-#     name: str
-#     message: str
-#     created_at: datetime = Field(default_factory=datetime.now, alias="createdAt")
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+class Payload(BaseModel):
+    """
+    Sample:
+    {
+        "name": "update deployment",
+        "message": "Successfully updated deployment default/wd (karolisr/webhook-demo:0.0.10)",
+        "createdAt": "2017-07-08T10:08:45.226565869+01:00"
+    }
+    """
+
+    name: str
+    message: str
+    created_at: datetime = Field(default_factory=datetime.now, alias="createdAt")
